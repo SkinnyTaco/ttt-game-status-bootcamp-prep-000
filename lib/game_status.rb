5505e14fb79c7a0955e9cab6e8c6967cde5f5afd
Daniel Dawson <dannyldawson@gmail.com>
2018-11-13 01:17:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,8 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     
-    if position_1 ==                          
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")    
+    end
   end 
 end
 
